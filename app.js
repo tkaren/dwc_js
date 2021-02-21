75007ea371fe556da6c9698c57ad486bcb36d846
tsunodakaren <k6432286@gmail.com>
2021-02-21 11:17:55 +0900

    [start] js

---
+++ app.js
@@ -0,0 +1,45 @@
+// var hello = 'Hello World';
+// alert(hello);
+
+// var int1 = 1;
+// alert(int1);
+
+// alert(4 + 3);
+
+// alert('Hello' + 'World');
+
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
+
+// var orange = 150;
+// var apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// var max = 200;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 4;
+//   count = count + 1;
+// }
+
+// alert('4を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
